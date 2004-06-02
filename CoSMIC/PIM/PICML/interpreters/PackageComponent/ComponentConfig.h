7346f0bff889145e37950efa974f2a7b2066dc4c
Krishnakumar Balasubramanian <kitty@dre.vanderbilt.edu>
2004-06-02 17:07:29 +0000

    ChangeLogTag: Wed Jun  2 12:03:02 2004  Krishnakumar B  <kitty@spam.invalid.domain>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@41 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/PICML/interpreters/PackageComponent/ComponentConfig.h
@@ -0,0 +1,43 @@
+// Component configuration file automatically generated as ComponentConfig.h
+// by UDM Interpreter Wizard on Monday, May 13, 2002 13:45:42
+
+#define RAWCOMPONENT
+
+
+// COM UUID-s, names and ProgID
+#define TYPELIB_UUID "a31e667f-0ad0-4421-b333-2610b3f3f25c"
+#define TYPELIB_NAME "MGA Interpreter TypeLibrary (PackageComponent)"
+#define COCLASS_UUID "61c41ffd-3a5e-468d-9b1e-bd2a89fed148"
+#define COCLASS_NAME "MGA Interpreter CoClass (PackageComponent)"
+#define COCLASS_PROGID "MGA.Interpreter.PackageComponent"
+
+
+// This name will appear in the popup window for interpreter selection.
+
+#define COMPONENT_NAME "PackageComponent"
+
+// This #define determines the interpreter type:
+#define GME_INTERPRETER
+#define PARADIGMS "PICML"
+
+// This is the location of the GME2000 interfaces file (Mga.idl, Meta.idl, etc)
+#define GME_INTERFACES_BASE C:/Program Files/GME/Interfaces
+
+#define GME_BASE C:/Program Files/GME
+
+#define TOOLTIP_TEXT "Generates Component Packaging & Assembly descriptors"
+
+#define BON_ICON_SUPPORT "CompIcon.ico"
+
+// Just to please the whims of those Microsoft jerks:
+#define COCLASS_UUID_EXPLODED1 0x61c41ffd
+#define COCLASS_UUID_EXPLODED2  0x3a5e
+#define COCLASS_UUID_EXPLODED3  0x468d
+#define COCLASS_UUID_EXPLODED4  0x9b
+#define COCLASS_UUID_EXPLODED5  0x1e
+#define COCLASS_UUID_EXPLODED6  0xbd
+#define COCLASS_UUID_EXPLODED7  0x2a
+#define COCLASS_UUID_EXPLODED8  0x89
+#define COCLASS_UUID_EXPLODED9  0xfe
+#define COCLASS_UUID_EXPLODED10  0xd1
+#define COCLASS_UUID_EXPLODED11  0x48
