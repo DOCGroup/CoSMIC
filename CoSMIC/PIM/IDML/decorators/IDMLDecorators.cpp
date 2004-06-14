1ae9b0c2fb5ab078d86e9c4bfb1de9d399534dc5
Jeff Parsons <j.parsons@vanderbilt.edu>
2004-06-14 23:10:48 +0000

    ChangeLogTag: Mon Jun 14 17:56:33 2004  Jeff Parsons <j.parsons@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@56 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/IDML/decorators/IDMLDecorators.cpp
@@ -0,0 +1,566 @@
+// Decorator.cpp : Implementation of CDecorator
+#include "stdafx.h"
+#include "IDMLDecorators.h"
+#include "Resource.h"
+#include "DecoratorUtil.h"
+#include "MaskedBitmap.h"
+#include <algorithm>
+
+CDecoratorUtil d_util;
+
+#define NAME_MARGINY 20
+
+#define ICON_SIZEX 64
+#define ICON_SIZEY 64
+
+static const int MAX_PORT_LENGTH = 3;
+static const int WIDTH_MODELSIDE = 100;
+static const int GAP_LABEL = 2;
+static const int GAP_PORT = 3;
+static const int GAP_XMODELPORT = 4;
+static const int GAP_YMODELPORT = 8;
+static const int GAP_XOVALICONFUDGE = 5;
+static const int GAP_YOVALICONFUDGE = 2;
+static const int GAP_PORTLABEL = 20;
+static const int WIDTH_MODEL = 113;
+static const int HEIGHT_MODEL = 71;
+
+//########################################################
+//
+//	CLASS : DecoratorBase
+//
+//########################################################
+
+DecoratorBase::DecoratorBase() 
+  : m_mgaFco( 0 ),
+    m_metaFco( 0 ),
+    m_color( GME_BLACK_COLOR ),
+    m_nameColor( GME_BLACK_COLOR )
+{
+}
+
+DecoratorBase::~DecoratorBase()
+{
+}
+
+void 
+DecoratorBase::initialize( IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco )
+{
+	m_mgaFco = obj;		// obj == NULL, if we are in the PartBrowser
+  m_metaFco = metaFco;
+	
+  CComBSTR bstr;
+  COMTHROW( m_metaFco->get_DisplayedName( &bstr ) );
+	if ( bstr.Length() == 0 ) {
+		bstr.Empty();
+		COMTHROW( m_metaFco->get_Name( &bstr ) );
+	}
+	m_metaName = bstr;
+
+  LoadBitmap();
+	
+	if ( m_mgaFco ) {
+		CComBSTR bstr;
+		COMTHROW( m_mgaFco->get_Name( &bstr ) );
+		m_name = bstr;
+	}
+	else {
+		CComBSTR bstr;
+		COMTHROW( m_metaFco->get_DisplayedName( &bstr ) );
+		if ( bstr.Length() == 0 ) {
+			bstr.Empty();
+			COMTHROW( m_metaFco->get_Name( &bstr ) );
+		}
+		m_name = bstr;
+	}
+}
+
+void
+DecoratorBase::destroy()
+{
+  m_metaFco = NULL;
+  m_mgaFco = NULL;
+}
+
+CSize
+DecoratorBase::getPreferredSize() const
+{
+  return CSize ( ICON_SIZEX, ICON_SIZEY );
+}
+
+void
+DecoratorBase::setLocation( const CRect& cRect )
+{
+  m_rect = cRect;
+}
+
+CRect
+DecoratorBase::getLocation() const
+{
+  return m_rect;
+}
+
+//########################################################
+//
+//	CLASS : MemberDecorator
+//
+//########################################################
+
+MemberDecorator::MemberDecorator()
+  : DecoratorBase()
+{
+}
+
+void 
+MemberDecorator::initialize( IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco )
+{
+  DecoratorBase::initialize( obj, metaFco );
+}
+
+void
+MemberDecorator::LoadBitmap()
+{
+  if (m_metaName == IDML_MEMBER_NAME 
+      || m_metaName == IDML_ATTRIBUTEMEMBER_NAME) {
+	  CComPtr<IMgaFCO> mgaFco = m_mgaFco;
+    if ( mgaFco ) {
+		  CComPtr<IMgaReference> ref;
+		  COMTHROW( mgaFco.QueryInterface( &ref ) );
+		  mgaFco = NULL;
+		  COMTHROW( ref->get_Referred( &mgaFco ) );
+    }
+		if (mgaFco) {
+      CComPtr<IMgaMetaFCO> metaFco;
+      mgaFco->get_Meta( &metaFco );
+		  CComBSTR bstr;
+		  COMTHROW( metaFco->get_Name( &bstr ) );
+      if ( bstr == IDML_STRING_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_STRING );
+      }
+      else if ( bstr == IDML_LONGINTEGER_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_LONG );       
+      }
+      else if ( bstr == IDML_BOOLEAN_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_BOOLEAN );       
+      }
+      else if ( bstr == IDML_REALNUMBER_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_REALNUMBER );       
+      }
+      else if ( bstr == IDML_COLLECTION_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_COLLECTIONREF );       
+      }
+      else if ( bstr == IDML_ENUM_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_ENUMREF );       
+      }
+      else if ( bstr == IDML_AGGREGATE_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_AGGREGATEREF );       
+      }
+      else if ( bstr == IDML_ALIAS_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_ALIASREF );       
+      }
+      else if ( bstr == IDML_SHORTINTEGER_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_SHORTINTEGER );       
+      }
+      else if ( bstr == IDML_OBJECT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_OBJECTREF );       
+      }
+      else if ( bstr == IDML_VALUEOBJECT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_VALUEREF );       
+      }
+      else if ( bstr == IDML_COMPONENT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_COMPONENTREF );       
+      }
+      else if ( bstr == IDML_EVENT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_EVENTREF );       
+      }
+      else if ( bstr == IDML_COMPONENTFACTORY_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_COMPONENTFACTORYREF );       
+      }
+      else if ( bstr == IDML_SWITCHEDAGGREGATE_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_SWITCHEDAGGREGATEREF );       
+      }
+      else if ( bstr == IDML_GENERICVALUE_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_GENERICVALUE );       
+      }
+      else if ( bstr == IDML_GENERICOBJECT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_GENERICOBJECT );       
+      }
+      else if ( bstr == IDML_GENERICVALUEOBJECT_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_GENERICVALUEOBJECT );       
+      }
+      else if ( bstr == IDML_BYTE_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_BYTE );       
+      }
+      else if ( bstr == IDML_TYPEENCODING_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_TYPEENCODING );       
+      }
+      else if ( bstr == IDML_TYPEKIND_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_TYPEKIND );       
+      }
+      else if ( bstr == IDML_BOXED_NAME ) {
+        m_bitmap.ReadFromResource( IDB_BITMAP_BOXEDREF );       
+      }
+		  mgaFco = NULL;
+    }
+    else {
+      m_bitmap.ReadFromResource( IDB_BITMAP_MEMBER );
+    }
+  }
+}
+
+void
+MemberDecorator::draw( CDC* pDC )
+{
+  if (m_bitmap.IsValid ()) {
+    m_bitmap.Draw( pDC, m_rect );
+
+    // Skip drawing the name only for attribute members appearing
+    // in the editor.
+    if (!m_mgaFco || m_metaName != IDML_ATTRIBUTEMEMBER_NAME) {
+		  CPoint namePos( m_rect.left + ICON_SIZEX / 2,
+                      m_rect.bottom + NAME_MARGINY );
+		  d_util.DrawText( pDC, 
+                       m_name,
+                       namePos,
+                       d_util.GetFont(GME_NAME_FONT),
+                       m_nameColor,
+                       TA_BOTTOM | TA_CENTER);
+    }
+  }
+}
+
+//########################################################
+//
+//	CLASS : PortDecorator
+//
+//########################################################
+
+PortDecorator::PortDecorator( CComPtr<IMgaFCO> mgaFco,
+                              const CPoint& ptInner )
+	: DecoratorBase(),
+    m_ptInner( ptInner ),
+    m_right( false )
+{
+  m_mgaFco = mgaFco;
+}
+
+void 
+PortDecorator::initialize()
+{
+  LoadBitmap();
+  CComBSTR bstr;
+  m_mgaFco->get_Name( &bstr );
+  m_name = bstr;
+  m_name = m_name.Left( MAX_PORT_LENGTH );
+}
+
+CSize 
+PortDecorator::getPreferredSize() const
+{
+	return CSize( GME_PORT_SIZE, GME_PORT_SIZE );
+}
+
+CPoint 
+PortDecorator::getInnerPosition() const
+{
+	return m_ptInner;
+}
+
+
+void
+PortDecorator::draw( CDC* pDC )
+{
+  CRect dst = getLocation();
+  m_bitmap.Draw( pDC, dst );
+
+	CPoint namePos( m_right ? dst.left - GAP_LABEL - ( 8 * MAX_PORT_LENGTH )
+                          : dst.right + GAP_LABEL,
+                  dst.top - GAP_PORT );
+	d_util.DrawText( pDC, 
+                   m_name,
+                   namePos,
+                   d_util.GetFont( GME_PORTNAME_FONT ),
+                   m_nameColor,
+                   TA_LEFT );
+}
+
+void
+PortDecorator::LoadBitmap()
+{
+	CComPtr<IMgaFCO> mgaFco = m_mgaFco;
+	if ( mgaFco ) {
+    CComPtr<IMgaMetaFCO> metaFco;
+    mgaFco->get_Meta( &metaFco );
+		CComBSTR bstr;
+		COMTHROW( metaFco->get_Name( &bstr ) );
+    if ( bstr == IDML_INEVENTPORT_NAME ) {
+      m_bitmap.ReadFromResource( m_right
+                                 ? IDB_BITMAP_INEVENT_RT
+                                 : IDB_BITMAP_INEVENT_LF );
+    }
+    else if ( bstr == IDML_OUTEVENTPORT_NAME ) {
+      m_bitmap.ReadFromResource( m_right
+                                 ? IDB_BITMAP_OUTEVENT_RT
+                                 : IDB_BITMAP_OUTEVENT_LF );
+    }
+    else if ( bstr == IDML_REQUIREDREQUESTPORT_NAME ) {
+      m_bitmap.ReadFromResource( m_right
+                                 ? IDB_BITMAP_RECEPTACLE_RT
+                                 : IDB_BITMAP_RECEPTACLE_LF );
+    }
+    else m_bitmap.ReadFromResource( IDB_BITMAP_FACET );
+  }
+}
+
+void
+PortDecorator::setToRight()
+{
+  m_right = true;
+}
+
+struct PortLess
+{
+	bool operator()( PortDecorator* pPortA, PortDecorator* pPortB )
+	{
+		return pPortA->getInnerPosition().y < pPortB->getInnerPosition().y;
+	}
+};
+
+//########################################################
+//
+//	CLASS : ComponentDecorator
+//
+//########################################################
+
+ComponentDecorator::ComponentDecorator( CComPtr<IMgaMetaPart>	metaPart )
+  : DecoratorBase(),
+    m_metaPart (metaPart),
+    m_iMaxPortTextLength( MAX_PORT_LENGTH )
+{
+}
+
+ComponentDecorator::~ComponentDecorator()
+{
+  unsigned int i;
+	for ( i = 0 ; i < m_vecLeftPorts.size() ; i++ )
+		delete m_vecLeftPorts[ i ];
+	for ( i = 0 ; i < m_vecRightPorts.size() ; i++ )
+		delete m_vecRightPorts[ i ];
+}
+
+void 
+ComponentDecorator::initialize(IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco)
+{
+  DecoratorBase::initialize(obj, metaFco);
+  if (m_mgaFco) loadPorts();
+}
+
+CSize
+ComponentDecorator::getPreferredSize() const
+{
+	if ( m_vecLeftPorts.empty() && m_vecRightPorts.empty() )
+		return DecoratorBase::getPreferredSize();
+
+	long lWidth =
+    ( 8 * m_iMaxPortTextLength
+      + GAP_LABEL
+      + GME_PORT_SIZE
+      + GAP_XMODELPORT
+      + GAP_XOVALICONFUDGE )
+      * 2
+    + GAP_PORTLABEL;
+	long lHeight =
+    (GAP_YMODELPORT + GAP_YOVALICONFUDGE) * 2
+    + max( m_vecLeftPorts.size(), m_vecRightPorts.size() )
+      * ( GME_PORT_SIZE + GAP_PORT )
+    - GAP_PORT;
+
+	return CSize( max( (long) WIDTH_MODEL, lWidth ),
+                max( (long) HEIGHT_MODEL, lHeight ) );
+}
+
+void
+ComponentDecorator::setLocation( const CRect& cRect )
+{
+  m_rect = cRect;
+	long lY = ( m_rect.Height()
+              - m_vecLeftPorts.size() * ( GME_PORT_SIZE + GAP_PORT )
+              + GAP_PORT )
+             / 2;
+
+  unsigned int i;
+
+	for ( i = 0 ; i < m_vecLeftPorts.size() ; i++ ) {
+		m_vecLeftPorts[ i ]->setLocation( CRect( GAP_XMODELPORT
+                                             + GAP_XOVALICONFUDGE, 
+                                             lY, 
+                                             GAP_XMODELPORT
+                                             + GME_PORT_SIZE
+                                             + GAP_XOVALICONFUDGE, 
+                                             lY + GME_PORT_SIZE ) );
+		lY += GME_PORT_SIZE + GAP_PORT;
+	}
+
+	lY = ( m_rect.Height()
+         - m_vecRightPorts.size() * ( GME_PORT_SIZE + GAP_PORT )
+         + GAP_PORT )
+        / 2;
+
+	for ( i = 0 ; i < m_vecRightPorts.size() ; i++ ) {
+		m_vecRightPorts[ i ]->setLocation( CRect( cRect.Width()
+                                              - GAP_XMODELPORT
+                                              - GME_PORT_SIZE
+                                              - GAP_XOVALICONFUDGE, 
+                                              lY,
+                                              cRect.Width()
+                                              - GAP_XMODELPORT
+                                              - GAP_XOVALICONFUDGE, 
+                                              lY + GME_PORT_SIZE ) );
+		lY += GME_PORT_SIZE + GAP_PORT;
+	}
+}
+
+void
+ComponentDecorator::LoadBitmap()
+{
+  m_bitmap.ReadFromResource(IDB_BITMAP_COMPONENT);       
+}
+
+void
+ComponentDecorator::draw(CDC* pDC)
+{
+  // Draw the component icon.
+  CRect dst = getLocation();
+	m_bitmap.Draw( pDC, dst );
+
+  // Draw the comonent name.
+	CPoint namePos(dst.left + dst.Width () / 2, dst.bottom + NAME_MARGINY);
+	d_util.DrawText(pDC, 
+                  m_name,
+                  namePos,
+                  d_util.GetFont(GME_NAME_FONT),
+                  m_nameColor,
+                  TA_BOTTOM | TA_CENTER);
+
+  // Draw the component ports, if any.
+	CSize cExtentD = pDC->GetViewportExt();
+	CSize cExtentL = pDC->GetWindowExt();
+	CRect cRect = getLocation();
+	CPoint ptOrigin = 
+    pDC->OffsetViewportOrg( (long) ( cRect.left
+                                     * ( (double) cExtentD.cx
+                                         / cExtentL.cx ) ),
+                            (long) ( cRect.top
+                                     * ( (double) cExtentD.cy
+                                         / cExtentL.cy ) ) );
+  unsigned int i;
+
+	for ( i = 0 ; i < m_vecLeftPorts.size() ; i++ ) {
+		m_vecLeftPorts[ i ]->draw( pDC );
+  }
+
+	for ( i = 0 ; i < m_vecRightPorts.size() ; i++ ) {
+		m_vecRightPorts[ i ]->draw( pDC );
+  }
+	pDC->SetViewportOrg( ptOrigin );
+}
+
+void
+ComponentDecorator::loadPorts()
+{
+	CComPtr<IMgaMetaAspect>	spParentAspect;
+	COMTHROW( m_metaPart->get_ParentAspect( &spParentAspect ) );
+
+	CComQIPtr<IMgaModel> spModel = m_mgaFco;
+	CComPtr<IMgaMetaFCO> spMetaFCO;
+	COMTHROW( spModel->get_Meta( &spMetaFCO ) );
+	CComQIPtr<IMgaMetaModel> spMetaModel = spMetaFCO;
+
+	CComBSTR bstrAspect;
+	COMTHROW( m_metaPart->get_KindAspect( &bstrAspect ) );
+	if ( bstrAspect.Length() == 0 ) {
+		bstrAspect.Empty();
+		COMTHROW( spParentAspect->get_Name( &bstrAspect ) );
+	}
+	
+	HRESULT hr = spMetaModel->get_AspectByName( bstrAspect, &m_spAspect );
+	
+	if ( hr == E_NOTFOUND) {
+		try {
+			// JEFF: There is only one aspect in IDML,
+      // but this is still the easiest way
+			m_spAspect = NULL;
+			CComPtr<IMgaMetaAspects> spAspects;
+			COMTHROW(spMetaModel->get_Aspects(&spAspects));
+			ASSERT(spAspects);
+			long nAspects = 0;
+			COMTHROW(spAspects->get_Count(&nAspects));
+			if (nAspects > 0) {
+				COMTHROW(spAspects->get_Item(1, &m_spAspect));
+			}
+		}
+		catch ( hresult_exception& ) {
+		}
+	}
+
+	if ( m_spAspect ) {
+
+		vector<PortDecorator*>	vecPorts;
+
+		CComPtr<IMgaFCOs> spFCOs;
+		COMTHROW( spModel->get_ChildFCOs( &spFCOs ) );
+		MGACOLL_ITERATE( IMgaFCO, spFCOs ) {
+			CComPtr<IMgaPart> spPart;
+			COMTHROW( MGACOLL_ITER->get_Part( m_spAspect, &spPart ) );
+			if ( spPart ) {
+				CComPtr<IMgaMetaPart> spMetaPart;
+				COMTHROW( spPart->get_Meta( &spMetaPart ) );
+				VARIANT_BOOL vbLinked;
+				COMTHROW( spMetaPart->get_IsLinked( &vbLinked ) );
+				if ( vbLinked ) {
+					long lX = 0;
+					long lY = 0;
+					COMTHROW( spPart->GetGmeAttrs( 0, &lX, &lY ) );
+					vecPorts.push_back( new PortDecorator( MGACOLL_ITER,
+                                                 CPoint( lX, lY ) ) );
+				}
+				else
+					COMTHROW( MGACOLL_ITER->Close() );
+			}
+			else
+				COMTHROW( MGACOLL_ITER->Close() );
+		} MGACOLL_ITERATE_END;
+
+		orderPorts( vecPorts );
+	}
+}
+
+void
+ComponentDecorator::orderPorts( vector<PortDecorator*>& vecPorts )
+{
+	long lMin = 100000000;
+	long lMax = 0;
+  unsigned int i;
+
+	for ( i = 0 ; i < vecPorts.size() ; i++ ) {
+		lMin = min( lMin, vecPorts[ i ]->getInnerPosition().x );
+		lMax = max( lMax, vecPorts[ i ]->getInnerPosition().x );
+	}
+
+	for ( i = 0 ; i < vecPorts.size() ; i++ ) {
+		if ( vecPorts[ i ]->getInnerPosition().x <= WIDTH_MODELSIDE 
+         || vecPorts[ i ]->getInnerPosition().x < lMax / 2 ) {
+			m_vecLeftPorts.push_back( vecPorts[ i ] );
+		}
+		else {
+			m_vecRightPorts.push_back( vecPorts[ i ] );
+      vecPorts[ i ]->setToRight();
+		}
+
+		vecPorts[ i ]->initialize();
+	}
+
+	sort( m_vecLeftPorts.begin(), m_vecLeftPorts.end(), PortLess() );
+	sort( m_vecRightPorts.begin(), m_vecRightPorts.end(), PortLess() );
+}
+
---
+++ CoSMIC/PIM/IDML/decorators/IDMLDecorators.h
@@ -0,0 +1,147 @@
+// IDMLDecorators.h : Declaration of the IDML Decorator classes
+
+#ifndef __IDMLDECORATOR_H_
+#define __IDMLDECORATOR_H_
+
+#pragma warning( disable : 4290 )
+
+#include "Resource.h"       // main symbols
+#include "DecoratorLib.h"
+#include "MaskedBitmap.h"
+#include <vector>
+
+using namespace std;
+
+#define IDML_AGGREGATE_NAME           "Aggregate"
+#define IDML_ALIAS_NAME               "Alias"
+#define IDML_ATTRIBUTEMEMBER_NAME     "AttributeMember"
+#define IDML_BOOLEAN_NAME             "Boolean"
+#define IDML_BOXED_NAME               "Boxed"
+#define IDML_BYTE_NAME                "Byte"
+#define IDML_COLLECTION_NAME          "Collection"
+#define IDML_COMPONENT_NAME           "Component"
+#define IDML_COMPONENTFACTORY_NAME    "ComponentFactory"
+#define IDML_ENUM_NAME                "Enum"
+#define IDML_EVENT_NAME               "Event"
+#define IDML_GENERICOBJECT_NAME       "GenericObject"
+#define IDML_GENERICVALUE_NAME        "GenericValue"
+#define IDML_GENERICVALUEOBJECT_NAME  "GenericValueObject"
+#define IDML_INEVENTPORT_NAME         "InEventPort"
+#define IDML_LONGINTEGER_NAME         "LongInteger"
+#define IDML_MEMBER_NAME              "Member"
+#define IDML_OBJECT_NAME              "Object"
+#define IDML_OUTEVENTPORT_NAME        "OutEventPort"
+#define IDML_PROVIDEDREQUESTPORT_NAME "ProvidedRequestPort"
+#define IDML_REALNUMBER_NAME          "RealNumber"
+#define IDML_REQUIREDREQUESTPORT_NAME "RequiredRequestPort"
+#define IDML_SHORTINTEGER_NAME        "ShortInteger"
+#define IDML_STRING_NAME              "String"
+#define IDML_SWITCHEDAGGREGATE_NAME   "SwitchedAggregate"
+#define IDML_TYPEENCODING_NAME        "TypeEncoding"
+#define IDML_TYPEKIND_NAME            "TypeKind"
+#define IDML_VALUEOBJECT_NAME         "ValueObject"
+
+//########################################################
+//
+//	CLASS : DecoratorBase
+//
+//########################################################
+
+class DecoratorBase
+{
+public:
+  virtual ~DecoratorBase();
+
+  virtual void      initialize( IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco );
+  virtual void      destroy();
+	virtual CSize			getPreferredSize() const;
+  virtual void      setLocation( const CRect& cRect );
+  virtual CRect     getLocation() const;
+  virtual void 			draw( CDC* pDC ) = 0;
+  virtual void      LoadBitmap() = 0;
+
+protected:
+  DecoratorBase();
+
+protected:
+  CRect                 m_rect;
+	CComPtr<IMgaFCO>	    m_mgaFco;
+	CComPtr<IMgaMetaFCO>	m_metaFco;
+	CString		            m_name;
+  CString               m_metaName;
+	CMaskedBitmap         m_bitmap;
+
+	COLORREF	            m_color;
+	COLORREF	            m_nameColor;
+};
+
+//########################################################
+//
+//	CLASS : MemberDecorator
+//
+//########################################################
+
+class MemberDecorator : public DecoratorBase
+{
+public:
+  MemberDecorator();
+
+  virtual void initialize( IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco );
+  virtual void draw( CDC* pDC );
+  virtual void LoadBitmap();
+};
+
+//########################################################
+//
+//	CLASS : PortDecorator
+//
+//########################################################
+
+class PortDecorator	: public DecoratorBase
+{
+private :
+	CPoint m_ptInner;
+  bool m_right;
+
+public :
+	PortDecorator( CComPtr<IMgaFCO> mgaFco, const CPoint& ptInner );
+
+	virtual void   initialize();
+	virtual CSize	 getPreferredSize() const;
+		      CPoint getInnerPosition() const;
+  virtual void 	 draw( CDC* pDC );
+  virtual void   LoadBitmap();
+          void   setToRight();
+};
+
+//########################################################
+//
+//	CLASS : ComponentDecorator
+//
+//########################################################
+
+class ComponentDecorator : public DecoratorBase
+{
+private:
+	CComPtr<IMgaMetaPart>	    m_metaPart;
+	vector<PortDecorator*>		m_vecLeftPorts;
+	vector<PortDecorator*>		m_vecRightPorts;
+	CComPtr<IMgaMetaAspect>	  m_spAspect;
+	long							        m_iMaxPortTextLength;
+
+public:
+  ComponentDecorator( CComPtr<IMgaMetaPart>	metaPart );
+  virtual ~ComponentDecorator();
+
+  virtual void  initialize( IMgaFCO *obj, CComPtr<IMgaMetaFCO>& metaFco );
+  virtual CSize getPreferredSize() const;
+  virtual void  setLocation( const CRect& cRect );
+  virtual void  draw( CDC* pDC );
+  virtual void  LoadBitmap();
+
+private:
+	void loadPorts();
+	void orderPorts( vector<PortDecorator*>& vecPorts );
+};
+
+#endif //__DECORATOR_H_
