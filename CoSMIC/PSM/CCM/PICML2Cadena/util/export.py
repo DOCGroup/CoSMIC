dbdc1f667037f9e2241f330ea292ddce2abbbf07
gtrombetti <gtrombetti>
2004-06-24 21:01:07 +0000

    Removed debug comments, code beautified, added support for -d and -dv at the beginning or in the middle of the commandline
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@111 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PSM/CCM/PICML2Cadena/util/export.py
+++ CoSMIC/PSM/CCM/PICML2Cadena/util/export.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-print ' ' #puts a newline
-
 #fetch needed environment variables
 gre_fullpathname = os.environ['GREAT_PATH'].rstrip('\\')+'\\bin\\GRE.exe'  #maybe not necessary because GReAT puts it into PATH
 integration_picml2cadena_fullpath = os.environ['PICML2CADENA_PATH']
@@ -15,13 +13,13 @@ if (integration_picml2cadena_fullpath=='') :
 #let's see if the user is looking for troubles...
 if (integration_picml2cadena_fullpath.find(' ')!=-1) : print 'Warning: To avoid troubles, put the PICML2Cadena directory in a path without whitespaces.\n\
 Current value is:\n'+integration_picml2cadena_fullpath+'\nGoing ahead...\n'
-
 integration_udm_fullpath = integration_picml2cadena_fullpath.rstrip('\\')+'\\Transformations\\Udm\\'  #this works regardless if the PICML2CADENA_PATH ends in '\' or not
 
 
-#fetching parameters. Expected first the PICML mga file and then the cadena_scenario xml file
+#fetching commandline parameters. Expected first the transformation configuration file,
+#then the PICML mga file and then the cadena_scenario xml file
 
-#Looking for help?
+#looking for help?
 if ('--help' in sys.argv or '-h' in sys.argv) :
     print '''Extensive help:
 
@@ -38,33 +36,31 @@ The filenames can be relative or absolute. GRE will be invoked with absolute fil
 Use ``export.py --help'' to get this extensive help'''
     sys.exit(0)
 
-#Bad invocation?
+#bad invocation?
 if (len(sys.argv)<4) :
-    print 'Usage: '+sys.argv[0]+' ConfigFile_of_Transformation.mga PICMLMgaFile.mga CadenaScenarioXMLFile.xml [-d | -dv]\nOr'+sys.argv[0]+' --help for extensive help'
+    print 'Usage: export.py ConfigFile_of_Transformation.mga PICMLMgaFile.mga CadenaScenarioXMLFile.xml [-d | -dv]\n\
+Or: export.py --help for extensive help'
     sys.exit(0)
-
-#Tries:
-#print gre_fullpathname
-#print integration_udm_fullpath
-#print len(sys.argv)  #Like this you access to argv and its length. Length of argv is like in C.
-#print os.path.abspath('goofy') #very useful!!
 
 quotes = '\"'
 space = ' '
+if ('-dv' in sys.argv) : debugflag = ' -dv'
+elif ('-d' in sys.argv) : debugflag = ' -d'
+else : debugflag = ''
+
+purifiedparams = []
+for el in sys.argv :
+    if (el!='-d' and el!='-dv') : purifiedparams.append(el)
+
 greinvocationcommand = 'gre.exe' + \
-                       space + quotes+os.path.abspath(sys.argv[1])+quotes + \
-                       space + quotes+os.path.abspath(sys.argv[2])+quotes
-if ('-dv' in sys.argv) : greinvocationcommand += ' -dv'
-elif ('-d' in sys.argv) : greinvocationcommand += ' -d'
+                       space + quotes+os.path.abspath(purifiedparams[1])+quotes + \
+                       space + quotes+os.path.abspath(purifiedparams[2])+quotes + \
+                       debugflag
 
 print 'Invoking GRE.exe with the following commandline:\n\
 _______________________________________________________________________________\n',\
 greinvocationcommand,'\n\
 -------------------------------------------------------------------------------\n'
 os.system(greinvocationcommand)
-
-#Another way to do it is:
-#(childstdin, childstdouterr) = os.popen4(greinvocationcommand)
-#for line in childstdouterr.readlines() : print line,  #trailing comma avoids extra newline
 
 
