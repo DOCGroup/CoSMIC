0f50d149cbb5a7c71bb7f49e5a88113991a8716b
Arvind Krishna <arvindk@dre.vanderbilt.edu>
2004-07-23 03:22:02 +0000

    ChangeLogTag: Thu Jul 22 22:15:04 CDT 2004 Arvind S. Krishna <arvindk@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@142 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/PICML/interpreters/cidl/Component/ComponentConfig.h
@@ -0,0 +1,45 @@
+// Component configuration file automatically generated as ComponentConfig.h
+// by UDM Interpreter Wizard on Wednesday, July 21, 2004 16:52:24
+
+#define RAWCOMPONENT
+
+// COM UUID-s, names and ProgID
+
+#define TYPELIB_UUID "15B7A57E-43EF-4B1A-AFEC-FC3D843659CF"
+#define TYPELIB_NAME "MGA Interpreter TypeLibrary (cidlc)"
+#define COCLASS_UUID "CC95C4DB-9E91-4702-9C9C-703A75A0EE98"
+#define COCLASS_NAME "MGA Interpreter CoClass (cidlc)"
+#define COCLASS_PROGID "MGA.Interpreter.cidlc"
+
+
+// This name will appear in the popup window for interpreter selection.
+#define COMPONENT_NAME "cidlc"
+
+
+// This #define determines the interpreter type:
+#define GME_INTERPRETER
+// The name of the paradigm(s). The GME will use this component
+// for this paradigm. Separate the names of paradigms by commas.
+#define PARADIGMS "PICML"
+
+#define TOOLTIP_TEXT "CIDL Generator"
+
+
+// This is the location of the GME interfaces file (Mga.idl, Meta.idl, etc)
+#define GME_INTERFACES_BASE C:/Program Files/GME/Interfaces
+
+
+#define BON_ICON_SUPPORT
+
+// Just to please the whims of those Microsoft jerks:
+#define COCLASS_UUID_EXPLODED1 0xCC95C4DB
+#define COCLASS_UUID_EXPLODED2  0x9E91
+#define COCLASS_UUID_EXPLODED3  0x4702
+#define COCLASS_UUID_EXPLODED4  0x9C
+#define COCLASS_UUID_EXPLODED5  0x9C
+#define COCLASS_UUID_EXPLODED6  0x70
+#define COCLASS_UUID_EXPLODED7  0x3A
+#define COCLASS_UUID_EXPLODED8  0x75
+#define COCLASS_UUID_EXPLODED9  0xA0
+#define COCLASS_UUID_EXPLODED10  0xEE
+#define COCLASS_UUID_EXPLODED11  0x98
