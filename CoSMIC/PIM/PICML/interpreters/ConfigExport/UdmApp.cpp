a377c9dc937fb4b8858cf3cc00ca7bfa3256cdae
Emre Turkay <turkaye@dre.vanderbilt.edu>
2005-04-10 19:58:12 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@433 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/PICML/interpreters/ConfigExport/UdmApp.cpp
+++ CoSMIC/PIM/PICML/interpreters/ConfigExport/UdmApp.cpp
@@ -35,6 +35,7 @@
 #include "UdmConfig.h"
 
 #include "PICML.h"
+#include <PICML/Utils.h>
 #include "ConfigExportVisitor.h"
 
 extern void dummy(void); // Dummy function for UDM meta initialization
@@ -45,59 +46,6 @@ extern void dummy(void); // Dummy function for UDM meta initialization
 int CUdmApp::Initialize()
 {
   return 0;
-}
-
-// This method prompts a dialog to allow the user to specify a folder
-static bool getPath (const std::string& description, std::string& path)
-{
-  // Initalize the com library
-  HRESULT hr = ::CoInitialize(NULL);
-  if (FAILED(hr))
-    return false;
-
-  // Dialog instruction
-  char display_buffer[MAX_PATH];
-  BROWSEINFO folder_browsinfo;
-  memset (&folder_browsinfo, 0, sizeof (folder_browsinfo));
-
-  // Set GME as the owner of the dialog
-  folder_browsinfo.hwndOwner = GetForegroundWindow();
-  // Start the browse from desktop
-  folder_browsinfo.pidlRoot = NULL;
-  // Pointer to the folder name display buffer
-  folder_browsinfo.pszDisplayName = &display_buffer[0];
-  // Dialog instruction string
-  folder_browsinfo.lpszTitle = description.c_str();
-  // Use new GUI style and allow edit plus file view
-  folder_browsinfo.ulFlags = BIF_RETURNONLYFSDIRS|BIF_USENEWUI;
-  // No callback function
-  folder_browsinfo.lpfn = NULL;
-  // No parameter passing into the dialog
-  folder_browsinfo.lParam = 0;
-
-  LPITEMIDLIST folder_pidl = SHBrowseForFolder(&folder_browsinfo);
-
-  if(folder_pidl == NULL)
-    return false;
-  else
-    {
-      TCHAR FolderNameBuffer[MAX_PATH];
-
-      // Convert the selection into a path
-      if (SHGetPathFromIDList (folder_pidl, FolderNameBuffer))
-        path = FolderNameBuffer;
-
-      // Free the ItemIDList object returned from the call to
-      // SHBrowseForFolder using Gawp utility function
-      IMalloc * imalloc = 0;
-      if ( SUCCEEDED( SHGetMalloc ( &imalloc )) )
-        {
-          imalloc->Free ( folder_pidl );
-          imalloc->Release ( );
-        }
-    }
-  ::CoUninitialize();
-  return true;
 }
 
 /*
@@ -141,7 +89,7 @@ void CUdmApp::UdmMain(Udm::DataNetwork* p_backend,      // Backend pointer
     {
       std::string outputPath;
       std::string message = "Please specify the Output Directory";
-      if (!getPath (message, outputPath))
+      if (!PICML::getPath (message, outputPath))
         return;
       PICML::ConfigExportVisitor visitor (outputPath);
       PICML::RootFolder root_folder =
