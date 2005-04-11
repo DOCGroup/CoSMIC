a5e261b856d6dfb2fb0087025cecdb2cb52553a6
Emre Turkay <turkaye@dre.vanderbilt.edu>
2005-04-11 14:02:33 +0000

    *** empty log message ***
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@440 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/OCML/engine/OCML_Engine_Export.h
+++ CoSMIC/PIM/OCML/engine/OCML_Engine_Export.h
@@ -1,13 +1,10 @@
-
 // -*- C++ -*-
 // $Id$
 // Definition for Win32 Export directives.
-// This file is generated automatically by generate_export_file.pl OCML_Engine
 // ------------------------------
+
 #ifndef OCML_ENGINE_EXPORT_H
 #define OCML_ENGINE_EXPORT_H
-
-#include "ace/config-all.h"
 
 #if !defined (OCML_ENGINE_HAS_DLL)
 #  define OCML_ENGINE_HAS_DLL 1
@@ -15,39 +12,13 @@
 
 #if defined (OCML_ENGINE_HAS_DLL) && (OCML_ENGINE_HAS_DLL == 1)
 #  if defined (OCML_ENGINE_BUILD_DLL)
-#    define OCML_Engine_Export ACE_Proper_Export_Flag
-#    define OCML_ENGINE_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)
-#    define OCML_ENGINE_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    define OCML_Engine_Export __declspec(dllexport)
 #  else /* OCML_ENGINE_BUILD_DLL */
-#    define OCML_Engine_Export ACE_Proper_Import_Flag
-#    define OCML_ENGINE_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)
-#    define OCML_ENGINE_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    define OCML_Engine_Export __declspec(dllimport)
 #  endif /* OCML_ENGINE_BUILD_DLL */
 #else /* OCML_ENGINE_HAS_DLL == 1 */
 #  define OCML_Engine_Export
-#  define OCML_ENGINE_SINGLETON_DECLARATION(T)
-#  define OCML_ENGINE_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #endif /* OCML_ENGINE_HAS_DLL == 1 */
-
-// Set OCML_ENGINE_NTRACE = 0 to turn on library specific tracing even if
-// tracing is turned off for ACE.
-#if !defined (OCML_ENGINE_NTRACE)
-#  if (ACE_NTRACE == 1)
-#    define OCML_ENGINE_NTRACE 1
-#  else /* (ACE_NTRACE == 1) */
-#    define OCML_ENGINE_NTRACE 0
-#  endif /* (ACE_NTRACE == 1) */
-#endif /* !OCML_ENGINE_NTRACE */
-
-#if (OCML_ENGINE_NTRACE == 1)
-#  define OCML_ENGINE_TRACE(X)
-#else /* (OCML_ENGINE_NTRACE == 1) */
-#  if !defined (ACE_HAS_TRACE)
-#    define ACE_HAS_TRACE
-#  endif /* ACE_HAS_TRACE */
-#  define OCML_ENGINE_TRACE(X) ACE_TRACE_IMPL(X)
-#  include "ace/Trace.h"
-#endif /* (OCML_ENGINE_NTRACE == 1) */
 
 #endif /* OCML_ENGINE_EXPORT_H */
 
--- CoSMIC/PIM/OCML/ui/CONFIGURATOR_UI_EXPORT.h
+++ CoSMIC/PIM/OCML/ui/CONFIGURATOR_UI_EXPORT.h
@@ -1,13 +1,10 @@
-
 // -*- C++ -*-
 // $Id$
 // Definition for Win32 Export directives.
-// This file is generated automatically by generate_export_file.pl CONFIGURATOR_UI
 // ------------------------------
+
 #ifndef CONFIGURATOR_UI_EXPORT_H
 #define CONFIGURATOR_UI_EXPORT_H
-
-#include "ace/config-all.h"
 
 #if !defined (CONFIGURATOR_UI_HAS_DLL)
 #  define CONFIGURATOR_UI_HAS_DLL 1
@@ -15,39 +12,13 @@
 
 #if defined (CONFIGURATOR_UI_HAS_DLL) && (CONFIGURATOR_UI_HAS_DLL == 1)
 #  if defined (CONFIGURATOR_UI_BUILD_DLL)
-#    define CONFIGURATOR_UI_Export ACE_Proper_Export_Flag
-#    define CONFIGURATOR_UI_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)
-#    define CONFIGURATOR_UI_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    define CONFIGURATOR_UI_Export __declspec(dllexport)
 #  else /* CONFIGURATOR_UI_BUILD_DLL */
-#    define CONFIGURATOR_UI_Export ACE_Proper_Import_Flag
-#    define CONFIGURATOR_UI_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)
-#    define CONFIGURATOR_UI_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    define CONFIGURATOR_UI_Export __declspec(dllimport)
 #  endif /* CONFIGURATOR_UI_BUILD_DLL */
 #else /* CONFIGURATOR_UI_HAS_DLL == 1 */
 #  define CONFIGURATOR_UI_Export
-#  define CONFIGURATOR_UI_SINGLETON_DECLARATION(T)
-#  define CONFIGURATOR_UI_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #endif /* CONFIGURATOR_UI_HAS_DLL == 1 */
-
-// Set CONFIGURATOR_UI_NTRACE = 0 to turn on library specific tracing even if
-// tracing is turned off for ACE.
-#if !defined (CONFIGURATOR_UI_NTRACE)
-#  if (ACE_NTRACE == 1)
-#    define CONFIGURATOR_UI_NTRACE 1
-#  else /* (ACE_NTRACE == 1) */
-#    define CONFIGURATOR_UI_NTRACE 0
-#  endif /* (ACE_NTRACE == 1) */
-#endif /* !CONFIGURATOR_UI_NTRACE */
-
-#if (CONFIGURATOR_UI_NTRACE == 1)
-#  define CONFIGURATOR_UI_TRACE(X)
-#else /* (CONFIGURATOR_UI_NTRACE == 1) */
-#  if !defined (ACE_HAS_TRACE)
-#    define ACE_HAS_TRACE
-#  endif /* ACE_HAS_TRACE */
-#  define CONFIGURATOR_UI_TRACE(X) ACE_TRACE_IMPL(X)
-#  include "ace/Trace.h"
-#endif /* (CONFIGURATOR_UI_NTRACE == 1) */
 
 #endif /* CONFIGURATOR_UI_EXPORT_H */
 
