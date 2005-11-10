726cbffd6de428f68b6434ddae14da4af67814c5
Jeff Parsons <j.parsons@vanderbilt.edu>
2005-11-10 17:59:24 +0000

    ChangeLogTag: Thu Nov 10 23:18:05 UTC 2005  Jeff Parsons <j.parsons@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@564 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/PICML/interpreters/IDLImporter/picml_visitor.cpp
+++ CoSMIC/PIM/PICML/interpreters/IDLImporter/picml_visitor.cpp
@@ -1423,8 +1423,8 @@ picml_visitor::visit_constant (AST_Constant *node)
         }
       else
         {
-          elem = this->doc_->createElement (X ("model"));
-          this->set_id_attr (elem, BE_GlobalData::MODEL);
+          elem = this->doc_->createElement (X ("reference"));
+          this->set_id_attr (elem, BE_GlobalData::REF);
         }
       
       // We add the elem to the table the first time the node is
