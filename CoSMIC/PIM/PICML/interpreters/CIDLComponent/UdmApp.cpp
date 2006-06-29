2d4e103cd36270db38bad360a6b0f98b9fe40b2a
Krishnakumar Balasubramanian <kitty@dre.vanderbilt.edu>
2006-06-29 05:43:32 +0000

    ChangeLogTag: Thu Jun 29 05:42:31 UTC 2006  Krishnakumar B  <kitty@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@665 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/PICML/interpreters/CIDLComponent/UdmApp.cpp
+++ CoSMIC/PIM/PICML/interpreters/CIDLComponent/UdmApp.cpp
@@ -13,52 +13,52 @@
 /* Initialize here the settings in the config global object.					  */
 /* Return 0 if successful.														  */
 /*********************************************************************************/
-#define SetUpVisitor(type, root, visitor)                               \
-  do                                                                    \
-    {                                                                   \
-      PICML:: ## type start = PICML:: ## type ## ::Cast (root);         \
-      start.Accept (visitor);                                           \
+#define SetUpVisitor(type, root, visitor)                       \
+  do                                                            \
+    {                                                           \
+      PICML:: ## type start = PICML:: ## type ## ::Cast (root); \
+      start.Accept (visitor);                                   \
     } while (0)
 
 int CUdmApp::Initialize()
 {
 
-	// TODO: Your initialization code comes here...
-	return 0;
+  // TODO: Your initialization code comes here...
+  return 0;
 }
 
 static void showUsage()
 {
-	AfxMessageBox ("The Model does not contain any Component IDL definitions: Interpretation Failed");
-	return;
+  AfxMessageBox ("The Model does not contain any Component IDL definitions: Interpretation Failed");
+  return;
 }
 
 
 
 /*
-Remarks to CUdmApp::UdmMain(...):
-0.	The p_backend points to an already open backend, and the framework
-closes it automatically. DO NOT OPEN OR CLOSE IT!
-To commit changes use p_backend->CommitEditSequence().
-To abort changes use p_backend->AbortEditSequence().
-To save changes to a different file use p_backend->SaveAs() or p_backend->CloseAs().
+  Remarks to CUdmApp::UdmMain(...):
+  0.	The p_backend points to an already open backend, and the framework
+  closes it automatically. DO NOT OPEN OR CLOSE IT!
+  To commit changes use p_backend->CommitEditSequence().
+  To abort changes use p_backend->AbortEditSequence().
+  To save changes to a different file use p_backend->SaveAs() or p_backend->CloseAs().
 
   1.	Focus is the currently open model.
 
-	2.	The possible values for param (from GME DecoratorLib.h component_startmode_enum):
-	GME_MAIN_START			=   0,
-	GME_BROWSER_START		=   1,
-	GME_CONTEXT_START		=   2,
-	GME_EMBEDDED_START		=   3,
-	GME_MENU_START			=  16,
-	GME_BGCONTEXT_START		=  18,
-	GME_ICON_START			=  32,
-	METAMODEL_CHECK_SYNTAX	= 101
+  2.	The possible values for param (from GME DecoratorLib.h component_startmode_enum):
+  GME_MAIN_START			=   0,
+  GME_BROWSER_START		=   1,
+  GME_CONTEXT_START		=   2,
+  GME_EMBEDDED_START		=   3,
+  GME_MENU_START			=  16,
+  GME_BGCONTEXT_START		=  18,
+  GME_ICON_START			=  32,
+  METAMODEL_CHECK_SYNTAX	= 101
 
-	  3. The framework catches all the exceptions and reports the error in a message box,
-	  clean up and close the transactions aborting the changes. You can override this
-	  behavior by catching udm_exception. Use udm_exception::what() to form an error
-	  message.
+  3. The framework catches all the exceptions and reports the error in a message box,
+  clean up and close the transactions aborting the changes. You can override this
+  behavior by catching udm_exception. Use udm_exception::what() to form an error
+  message.
 */
 
 /***********************************************/
@@ -66,110 +66,111 @@ To save changes to a different file use p_backend->SaveAs() or p_backend->CloseA
 /***********************************************/
 
 void CUdmApp::UdmMain(
-					  Udm::DataNetwork* p_backend,		// Backend pointer(already open!)
-					  Udm::Object focusObject,			// Focus object
-					  set<Udm::Object> selectedObjects,	// Selected objects
-					  long param)						// Parameters
+                      Udm::DataNetwork* p_backend,		// Backend pointer(already open!)
+                      Udm::Object focusObject,			// Focus object
+                      set<Udm::Object> selectedObjects,	// Selected objects
+                      long param)						// Parameters
 {
-	try
+  try
     {
-		std::string outputPath;
-		std::string message = "Please specify the Output Directory";
+      std::string outputPath;
+      std::string message = "Please specify the Output Directory";
 
-		// If there is no output path specified
-		if (! PICML::getPath (message, outputPath))
-			return;
+      // If there is no output path specified
+      if (! PICML::getPath (message, outputPath))
+        return;
 
-		bool valid_interpretation = 0;
+      bool valid_interpretation = 0;
 
-		if (focusObject != Udm::null ||
-			!selectedObjects.empty())
+      if (focusObject != Udm::null ||
+          !selectedObjects.empty())
         {
-			std::set<Udm::Object> mySet (selectedObjects);
-			if (focusObject != Udm::null)
-				mySet.insert (focusObject);
+          std::set<Udm::Object> mySet (selectedObjects);
+          if (focusObject != Udm::null)
+            mySet.insert (focusObject);
 
-			for (std::set<Udm::Object>::iterator iter = mySet.begin();
-			iter != mySet.end();
-			++iter)
+          for (std::set<Udm::Object>::iterator iter = mySet.begin();
+               iter != mySet.end();
+               ++iter)
             {
-				// Interpretation should start from Benchmarking Aspect
-				Udm::Object root = *iter;
-				std::string kindName = (*iter).type().name();
-				// Generate cidl file only for the Component
-				if (kindName == "File")
-				{
-					Udm::Object root = *iter;
-					PICML::Cidlc_Visitor visitor (outputPath);
-					SetUpVisitor (File, root, visitor);
-					valid_interpretation = 1;
-				}
+              // Interpretation should start from Benchmarking Aspect
+              Udm::Object root = *iter;
+              std::string kindName = (*iter).type().name();
+              // Generate cidl file only for the Component
+              if (kindName == "File")
+                {
+                  Udm::Object root = *iter;
+                  PICML::Cidlc_Visitor visitor (outputPath);
+                  SetUpVisitor (File, root, visitor);
+                  valid_interpretation = 1;
+                }
 
-			}
-		}
+            }
+        }
 
-		if (! valid_interpretation)
-		{
-			Udm::Object root_obj = p_backend->GetRootObject();
-			PICML::RootFolder root_folder = PICML::RootFolder::Cast (root_obj);
-			std::set<PICML::InterfaceDefinitions> idefs = root_folder.InterfaceDefinitions_children();
-			for (std::set<PICML::InterfaceDefinitions>::iterator iter = idefs.begin ();
-			iter != idefs.end (); iter ++)
-			{
-				// Find out the IDL files that are present in each Interface definition
-				std::set<PICML::File> idl_files = iter->File_children();
-				for (std::set<PICML::File>::iterator iter2 = idl_files.begin ();
-					 iter2 != idl_files.end ();
-					 iter2 ++)
+      if (! valid_interpretation)
+        {
+          Udm::Object root_obj = p_backend->GetRootObject();
+          PICML::RootFolder root_folder = PICML::RootFolder::Cast (root_obj);
+          std::set<PICML::InterfaceDefinitions> idefs = root_folder.InterfaceDefinitions_children();
+          for (std::set<PICML::InterfaceDefinitions>::iterator iter = idefs.begin ();
+               iter != idefs.end (); iter ++)
+            {
+              // Find out the IDL files that are present in each Interface definition
+              std::set<PICML::File> idl_files = iter->File_children();
+              for (std::set<PICML::File>::iterator iter2 = idl_files.begin ();
+                   iter2 != idl_files.end ();
+                   iter2 ++)
 
-				{
-					// Generate the cidl files
-					PICML::Cidlc_Visitor visitor (outputPath);
-					SetUpVisitor (File,*iter2,visitor);
-					valid_interpretation = 1;
+                {
+                  // Generate the cidl files
+                  PICML::Cidlc_Visitor visitor (outputPath);
+                  SetUpVisitor (File,*iter2,visitor);
+                  valid_interpretation = 1;
 
-				}
+                }
 
-			}
-		}
+            }
+        }
 
-		if (valid_interpretation)
-			AfxMessageBox ("Cidl files successfully generated");
-		else
-			showUsage ();
+      if (valid_interpretation)
+        AfxMessageBox ("Cidl files successfully generated",
+                       MB_OK| MB_ICONINFORMATION);
+      else
+        showUsage ();
     }
 
-	catch(udm_exception &e)
+  catch(udm_exception &e)
     {
-		AfxMessageBox(e.what());
+      AfxMessageBox(e.what());
     }
 
-	return;
+  return;
 
 #ifdef _DEBUG
-	// Displaying the name of the root object meta
-	Udm::Object rootObject=p_backend->GetRootObject();
-	string rootObjectName("Root Object's Class Name: ");
-	rootObjectName+=ExtractName(rootObject.type());
-	AfxMessageBox(rootObjectName.c_str());
+  // Displaying the name of the root object meta
+  Udm::Object rootObject=p_backend->GetRootObject();
+  string rootObjectName("Root Object's Class Name: ");
+  rootObjectName+=ExtractName(rootObject.type());
+  AfxMessageBox(rootObjectName.c_str());
 
-	// Displaying the focus object
-	string focusObjectName("Focus Object Name: ");
-	focusObjectName+=ExtractName(focusObject);
-	AfxMessageBox(focusObjectName.c_str());
+  // Displaying the focus object
+  string focusObjectName("Focus Object Name: ");
+  focusObjectName+=ExtractName(focusObject);
+  AfxMessageBox(focusObjectName.c_str());
 
-	// Displaying selected objects
-	string selObjNames("Selected Objects:\r\n");
-	// Iterate set
-	for(set<Udm::Object>::iterator i=selectedObjects.begin();
-	i!=selectedObjects.end();i++)
-	{
-		selObjNames+=ExtractName(*i);
-		selObjNames+="\r\n";
-	}
-	AfxMessageBox(selObjNames.c_str());
+  // Displaying selected objects
+  string selObjNames("Selected Objects:\r\n");
+  // Iterate set
+  for(set<Udm::Object>::iterator i=selectedObjects.begin();
+      i!=selectedObjects.end();i++)
+    {
+      selObjNames+=ExtractName(*i);
+      selObjNames+="\r\n";
+    }
+  AfxMessageBox(selObjNames.c_str());
 #endif
-	/****************************** Demo Code End *************************/
+  /****************************** Demo Code End *************************/
 
 }
 
@@ -184,26 +185,26 @@ void CUdmApp::UdmMain(
 /*****************************************************/
 string CUdmApp::ExtractName(Udm::Object ob)
 {
-	Uml::Class cls= ob.type();
-	set<Uml::Attribute> attrs=cls.attributes();
+  Uml::Class cls= ob.type();
+  set<Uml::Attribute> attrs=cls.attributes();
 
-	// Adding parent attributes
-	set<Uml::Attribute> aattrs=Uml::AncestorAttributes(cls);
-	attrs.insert(aattrs.begin(),aattrs.end());
+  // Adding parent attributes
+  set<Uml::Attribute> aattrs=Uml::AncestorAttributes(cls);
+  attrs.insert(aattrs.begin(),aattrs.end());
 
-	for(set<Uml::Attribute>::iterator ai = attrs.begin();ai != attrs.end(); ai++)
-	{
-		if(string(ai->type())=="String")
-		{
-			string str=ai->name();
-			if(str=="name")
-			{
-				string value=ob.getStringAttr(*ai);
-				if(value.empty())value="<empty string>";
-				return value;
-			}
-		}
-	}
-	return string("<no name specified>");
+  for(set<Uml::Attribute>::iterator ai = attrs.begin();ai != attrs.end(); ai++)
+    {
+      if(string(ai->type())=="String")
+        {
+          string str=ai->name();
+          if(str=="name")
+            {
+              string value=ob.getStringAttr(*ai);
+              if(value.empty())value="<empty string>";
+              return value;
+            }
+        }
+    }
+  return string("<no name specified>");
 }
 #endif
