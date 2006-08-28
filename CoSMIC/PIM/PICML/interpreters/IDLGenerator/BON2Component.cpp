4b721fdc7111f626ed15e4793df263e373b2bb0c
Krishnakumar Balasubramanian <kitty@dre.vanderbilt.edu>
2006-08-28 16:20:32 +0000

    ChangeLogTag: Mon Aug 28 16:18:52 UTC 2006  Krishnakumar B  <kitty@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@774 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/PICML/interpreters/IDLGenerator/BON2Component.cpp
+++ CoSMIC/PIM/PICML/interpreters/IDLGenerator/BON2Component.cpp
@@ -150,7 +150,6 @@ void Component::invokeEx( Project& project,
                           const std::set<FCO>& setSelectedFCOs,
                           long lParam )
 {
-  project->setNmsp("PICML");
   std::string outputPath;
   std::string message = "Please specify the Output Directory";
 
