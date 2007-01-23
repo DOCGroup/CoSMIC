617f194dc49bc7d503f93e2aaf85c4d82d914478
James Hill <hillj@cs.iupui.edu>
2007-01-23 01:01:57 +0000

    Tue Jan 23 01:00:53 UTC 2007  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1073 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/CUTS/integration/vc8.bat
+++ CoSMIC/CUTS/integration/vc8.bat
@@ -4,12 +4,5 @@
 
 @setlocal
 @call "%VS80COMNTOOLS%vsvars32.bat"
-
-@ant -buildfile %CUTS_ROOT%\integration\autobuild.build ^
--Dmpc.type=vc8 ^
--Dresults.log=%CUTS_ROOT%\integration\logs\vc8.build.log ^
--logfile %CUTS_ROOT%\integration\logs\vc8.build.xml ^
--logger org.apache.tools.ant.DefaultLogger ^
-build
-
+@call ant.bat -Dmpc.type=vc8 -Dresults.log=%CUTS_ROOT%\integration\logs\vc8.build.log %*
 @endlocal
