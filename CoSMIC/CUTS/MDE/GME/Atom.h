c514b37c599c03469fa9c04ae1830de31c3bdda0
James Hill <hillj@cs.iupui.edu>
2007-01-29 15:37:27 +0000

    Mon Jan 29 15:36:56 UTC 2007  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1099 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/CUTS/MDE/GME/Atom.h
+++ CoSMIC/CUTS/MDE/GME/Atom.h
@@ -1,3 +1,15 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ * @file      Atom.h
+ *
+ * $Id$
+ *
+ * @author    James H. Hill
+ */
+//=============================================================================
+
 #ifndef _GME_ATOM_H_
 #define _GME_ATOM_H_
 
@@ -5,22 +17,60 @@
 
 namespace GME
 {
+  //===========================================================================
+  /**
+   * @class Atom
+   *
+   * Wrapper class for the IMgaAtom interface.
+   */
+  //===========================================================================
+
   class GME_Export Atom : public FCO
   {
   public:
+    /// Type definition for the interface.
     typedef IMgaAtom _type;
 
+    /// Default constructor.
     Atom (void);
+
+    /**
+     * Initializing constructor.
+     *
+     * @param[in]       atom        Pointer to the atom.
+     */
     Atom (IMgaAtom * atom);
+
+    /**
+     * Copy constructor.
+     *
+     * @param[in]       atom        The source object.
+     */
     Atom (const Atom & atom);
+
+    /// Destructor.
     virtual ~Atom (void);
 
+    /**
+     * Assignment operator.
+     *
+     * @param[in]       atom        The source object.
+     * @return          Reference to this object.
+     */
     const Atom & operator = (const Atom & atom);
+
+    /**
+     * Attach to an existing atom.
+     *
+     * @param[in]       atom        The source interface.
+     */
     void attach (IMgaAtom * atom);
 
   protected:
+    /// Helper method to get the correct implementation.
     IMgaAtom * impl (void) const;
 
+    /// The underlying interface pointer.
     mutable CComPtr <IMgaAtom> atom_;
   };
 }
