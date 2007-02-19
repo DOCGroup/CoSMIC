bdc354a1a7496fc37b4a7c289b131bcac6d83ef2
James Hill <hillj@cs.iupui.edu>
2007-02-19 18:30:04 +0000

    Mon Feb 19 18:29:13 UTC 2007  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1185 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/CUTS/tests/cuts/workers/CPU_Worker_Test.cpp
@@ -0,0 +1,52 @@
+// -*- C++ -*-
+// $Id$
+
+#include "Test_Suite.h"
+#include "cuts/CPUWorker.h"
+#include "ace/OS_NS_sys_time.h"
+#include "ace/Log_Msg.h"
+#include "ace/Time_Value.h"
+#include "ace/High_Res_Timer.h"
+#include "ace/Thread.h"
+#include "ace/Malloc_T.h"
+#include "ace/Null_Mutex.h"
+#include <math.h>
+
+CUTS_UNIT_TEST (Unit_Test_CPU_run)
+{
+  CUTS_CPU_Worker worker;
+  ACE_High_Res_Timer timer;
+  ACE_Time_Value elapsed;
+
+  for (size_t i = 0; i < 100; i ++)
+  {
+    size_t msec = 500;
+    size_t cycles;
+
+    timer.start ();
+    cycles = worker.run (msec);
+    timer.stop ();
+
+    timer.elapsed_time (elapsed);
+    ACE_DEBUG ((LM_DEBUG,
+                "proposed time = %d; elapsed time = %d; diff = %d msec "
+                "(cycles = %d)\n",
+                msec,
+                elapsed.msec (),
+                msec - elapsed.msec (),
+                cycles));
+  }
+
+  return 0;
+}
+
+//=============================================================================
+/*
+ * CUTS_CPU_Worker_TS
+ */
+//=============================================================================
+
+CUTS_TEST_SUITE_BEGIN (CUTS_CPU_Worker_TS, CUTS_CPU_Worker)
+  CUTS_ADD_UNIT_TEST ("CPU_run", Unit_Test_CPU_run)
+CUTS_TEST_SUITE_END (CUTS_CPU_Worker_TS)
+
