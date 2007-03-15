8f3c5d5fbaa2d7968916bac93b29a001989c5b2d
Sumant Tambe <sutambe@dre.vanderbilt.edu>
2007-03-15 04:55:23 +0000

    Thu Mar 15 04:53:01 UTC 2007  Sumant Tambe  <sutambe@nospam>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1247 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/Installer/README.txt
@@ -0,0 +1,6 @@
+How to create the list of icons:
+
+1. Install Unix Utils for windows. Specifically "sh".
+2. Run the following command from $COSMIC_ROOT
+   sh Installer\list_icons.sh PICML PIM\\PICML\\paradigms\\icons
+
---
+++ CoSMIC/Installer/list_icons.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+count=1
+icon=_icon_
+for f in $( ls $2 ); do
+  str="<File Id='$1$icon$count'"
+  echo $str
+  str="      Name='f$count'"
+  echo $str
+  str="      LongName='$f'"
+  echo $str
+  str="      DiskId='1'"
+  echo $str
+  str="      Source='\$(var.COSMIC_ROOT)\\$2\\$f' />"
+  echo $str
+  echo " "
+
+  let count=count+1
+done
+
