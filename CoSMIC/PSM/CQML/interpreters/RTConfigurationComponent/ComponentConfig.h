408554ccc6101a86c55a1f245231636f877431e4
Amogh Kavimandan <amoghk@dre.vanderbilt.edu>
2007-04-12 21:06:31 +0000

    Thu Apr 12 20:50:15 UTC 2007  Amogh Kavimandan  <amoghk@nospam>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1339 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PSM/CQML/interpreters/RTConfigurationComponent/ComponentConfig.h
@@ -0,0 +1,52 @@
+// Component configuration file automatically generated as ComponentConfig.h
+// by ConfigureComponent on Thu Apr 05 10:43:02 2007
+
+
+#define RAWCOMPONENT
+
+// COM UUID-s, names and progID
+
+#define TYPELIB_UUID "87E7CBD8-5734-4F8C-84BA-DBBB00C4101F"
+#define TYPELIB_NAME "MGA Interpreter TypeLibrary (RTConfiguration)"
+#define COCLASS_UUID "D89ECDF9-A314-47A7-8115-640F807E743D"
+#define COCLASS_NAME "MGA Interpreter CoClass (RTConfiguration)"
+#define COCLASS_PROGID "MGA.Interpreter.RTConfiguration"
+
+
+// This name will appear in the popup window for interpreter selection.
+#define COMPONENT_NAME "RTConfiguration"
+
+
+// This text will appear in the toolbar icon tooltip and in the menu.
+#define TOOLTIP_TEXT "RTConfiguration Interpreter"
+
+
+// This #define determines the interpreter type:
+#define GME_INTERPRETER
+// The name of the paradigm(s). The GME will use this component
+// for this paradigm. Separate the names of paradigms by commas.
+#define PARADIGMS "RTConfiguration"
+
+
+// This is the location of the GME interfaces file (Mga.idl, Meta.idl, etc)
+#define GME_BASE . 
+
+
+#define BON_ICON_SUPPORT
+
+// not defined: #define BON_CUSTOM_TRANSACTIONS
+
+// not defined: #define REGISTER_SYSTEMWIDE
+
+// Just to please the whims of those Microsoft jerks:
+#define COCLASS_UUID_EXPLODED1 0xD89ECDF9
+#define COCLASS_UUID_EXPLODED2  0xA314
+#define COCLASS_UUID_EXPLODED3  0x47A7
+#define COCLASS_UUID_EXPLODED4  0x81
+#define COCLASS_UUID_EXPLODED5  0x15
+#define COCLASS_UUID_EXPLODED6  0x64
+#define COCLASS_UUID_EXPLODED7  0x0F
+#define COCLASS_UUID_EXPLODED8  0x80
+#define COCLASS_UUID_EXPLODED9  0x7E
+#define COCLASS_UUID_EXPLODED10  0x74
+#define COCLASS_UUID_EXPLODED11  0x3D
