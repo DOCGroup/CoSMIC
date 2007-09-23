e279287cd66971a9900f7c7502c6ac845e0f1eaa
Sumant Tambe <sutambe@dre.vanderbilt.edu>
2007-09-23 00:09:14 +0000

    Sun Sep 23 00:08:22 UTC 2007  Sumant Tambe  <sutambe@nospam>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1566 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/CQML/interpreters/ECLGenerator/ComponentConfig.h
+++ CoSMIC/PIM/CQML/interpreters/ECLGenerator/ComponentConfig.h
@@ -1,14 +1,14 @@
 // Component configuration file automatically generated as ComponentConfig.h
-// by ConfigureComponent on Fri Sep 21 20:02:40 2007
+// by ConfigureComponent on Sat Sep 22 19:04:42 2007
 
 
 #define BON2COMPONENT
 
 // COM UUID-s, names and progID
 
-#define TYPELIB_UUID "165C3F73-D9A2-4590-9691-B2961F3DC197"
+#define TYPELIB_UUID "2D72792A-273D-4F29-95AA-BCE041AD200E"
 #define TYPELIB_NAME "MGA Interpreter TypeLibrary (ECLGenerator)"
-#define COCLASS_UUID "4042BEF5-D77C-4B0A-A8FC-344C1D6CE00C"
+#define COCLASS_UUID "43A22705-E6B6-4B4B-81D1-F2E2B8F34C26"
 #define COCLASS_NAME "MGA Interpreter CoClass (ECLGenerator)"
 #define COCLASS_PROGID "MGA.Interpreter.ECLGenerator"
 
@@ -30,21 +30,21 @@
 #define GME_BASE C:/Program Files/GME
 
 
-// not defined: #define BON_ICON_SUPPORT
+#define BON_ICON_SUPPORT
 
 // not defined: #define BON_CUSTOM_TRANSACTIONS
 
 // not defined: #define REGISTER_SYSTEMWIDE
 
 // Just to please the whims of those Microsoft jerks:
-#define COCLASS_UUID_EXPLODED1 0x4042BEF5
-#define COCLASS_UUID_EXPLODED2  0xD77C
-#define COCLASS_UUID_EXPLODED3  0x4B0A
-#define COCLASS_UUID_EXPLODED4  0xA8
-#define COCLASS_UUID_EXPLODED5  0xFC
-#define COCLASS_UUID_EXPLODED6  0x34
-#define COCLASS_UUID_EXPLODED7  0x4C
-#define COCLASS_UUID_EXPLODED8  0x1D
-#define COCLASS_UUID_EXPLODED9  0x6C
-#define COCLASS_UUID_EXPLODED10  0xE0
-#define COCLASS_UUID_EXPLODED11  0x0C
+#define COCLASS_UUID_EXPLODED1 0x43A22705
+#define COCLASS_UUID_EXPLODED2  0xE6B6
+#define COCLASS_UUID_EXPLODED3  0x4B4B
+#define COCLASS_UUID_EXPLODED4  0x81
+#define COCLASS_UUID_EXPLODED5  0xD1
+#define COCLASS_UUID_EXPLODED6  0xF2
+#define COCLASS_UUID_EXPLODED7  0xE2
+#define COCLASS_UUID_EXPLODED8  0xB8
+#define COCLASS_UUID_EXPLODED9  0xF3
+#define COCLASS_UUID_EXPLODED10  0x4C
+#define COCLASS_UUID_EXPLODED11  0x26
