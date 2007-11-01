cc4030549e80e3006fe7585336cdd919409914a6
James Hill <hillj@cs.iupui.edu>
2007-11-01 17:09:12 +0000

    Thu Nov  1 17:06:52 UTC 2007  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1634 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/CUTS/integration/vc71.bat
+++
@@ -1,8 +0,0 @@
-@rem *********************************************
-@rem ** Build the vc71 version of CUTS
-@rem *********************************************
-
-@setlocal
-@call "%VS71COMNTOOLS%vsvars32.bat"
-@call ant.bat -Dmpc.type=vc71 %*
-@endlocal
--- CoSMIC/CUTS/integration/vc8.bat
+++
@@ -1,8 +0,0 @@
-@rem *********************************************
-@rem ** Build the vc8 version of CUTS
-@rem *********************************************
-
-@setlocal
-@call "%VS80COMNTOOLS%vsvars32.bat"
-@call ant.bat -Dmpc.type=vc8 %*
-@endlocal
