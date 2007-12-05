616edaccfa8286bbb74caa24d50874df20990742
James Hill <hillj@cs.iupui.edu>
2007-12-05 14:46:10 +0000

    Wed Dec  5 14:45:25 UTC 2007  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1701 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/Utils/gme/stdafx.h
@@ -0,0 +1,24 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ * @file        stdafx.h
+ *
+ * $Id$
+ *
+ * @author      James H. Hill
+ */
+//=============================================================================
+
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0400
+#endif
+
+#endif  // !defined _STDAFX_H_
