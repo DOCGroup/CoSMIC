6a86f15e8bd1da8c404c129135403ad4568f3e26
James Hill <hillj@cs.iupui.edu>
2008-02-21 07:09:03 +0000

    Thu Feb 21 07:08:24 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1762 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/Utils/gme/GME_export.h
+++ CoSMIC/Utils/gme/GME_export.h
@@ -1,20 +1,26 @@
 // -*- C++ -*-
 // $Id$
 
-#ifndef GME_EXPORT_H
-#define GME_EXPORT_H
+#ifndef _GME_EXPORT_H_
+#define _GME_EXPORT_H_
 
-#if !defined (GME_HAS_DLL)
-#  define GME_HAS_DLL 1
-#endif /* ! GME_HAS_DLL */
+#if defined (GME_AS_STATIC_LIBS)
+# if !defined (GME_HAS_DLL)
+#   define GME_HAS_DLL 0
+# endif /* !defined GME_HAS_DLL  */
+#else
+# if !defined (GME_HAS_DLL)
+#   define GME_HAS_DLL 1
+# endif /* !defined GME_HAS_DLL  */
+#endif  /* GME_AS_STATIC_LIBS    */
 
 #if defined (GME_HAS_DLL) && (GME_HAS_DLL == 1)
 #  if defined (GME_BUILD_DLL)
 #    define GME_Export __declspec (dllexport)
-#  else /* GME_BUILD_DLL */
+#  else   /* GME_BUILD_DLL */
 #    define GME_Export __declspec (dllimport)
-#  endif /* GME_BUILD_DLL */
-#else /* GME_HAS_DLL == 1 */
+#  endif  /* GME_BUILD_DLL */
+#else  /* GME_HAS_DLL == 1 */
 #  define GME_Export
 #endif /* GME_HAS_DLL == 1 */
 
@@ -24,4 +30,4 @@
 #define GME_INLINE
 #endif
 
-#endif /* GME_EXPORT_H */
+#endif /* !defined _GME_EXPORT_H_ */
