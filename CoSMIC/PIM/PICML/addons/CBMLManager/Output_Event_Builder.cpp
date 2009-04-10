55d3aaa786ea1bd4ece670780127f0aa09390185
James Hill <hillj@cs.iupui.edu>
2009-04-10 15:25:24 +0000

    Fri Apr 10 14:04:06 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1911 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/PICML/addons/CBMLManager/Output_Event_Builder.cpp
@@ -0,0 +1,22 @@
+// $Id$
+
+#include "StdAfx.h"
+#include "Output_Event_Builder.h"
+
+//
+// CBML_Output_Event_Builder
+//
+CBML_Output_Event_Builder::
+CBML_Output_Event_Builder (GME::Model & output)
+: output_ (output)
+{
+
+}
+
+//
+// ~CBML_Output_Event_Builder
+//
+CBML_Output_Event_Builder::~CBML_Output_Event_Builder (void)
+{
+
+}
---
+++ CoSMIC/PIM/PICML/addons/CBMLManager/Output_Event_Builder.h
@@ -0,0 +1,32 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ * @file        Output_Event_Builder.h
+ *
+ * $Id$
+ *
+ * @author      James H. Hill
+ */
+//=============================================================================
+
+#ifndef _OUTPUT_EVENT_BUILDER_H_
+#define _OUTPUT_EVENT_BUILDER_H_
+
+#include "game/GME_fwd.h"
+
+/**
+ * @class CBML_Output_Event_Builder
+ */
+class CBML_Output_Event_Builder
+{
+public:
+  CBML_Output_Event_Builder (GME::Model & output);
+
+  ~CBML_Output_Event_Builder (void);
+
+private:
+  GME::Model & output_;
+};
+
+#endif  // !defined _OUTPUT_EVENT_BUILDER_H_
