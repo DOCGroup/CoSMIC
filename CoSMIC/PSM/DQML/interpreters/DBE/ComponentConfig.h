29d12432369ebca2c617ad3278878ea82339ac5f
Joe Hoffert <jhoffert@dre.vanderbilt.edu>
2009-07-23 22:49:51 +0000

    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1950 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PSM/DQML/interpreters/DBE/ComponentConfig.h
+++ CoSMIC/PSM/DQML/interpreters/DBE/ComponentConfig.h
@@ -8,17 +8,18 @@
 
 #define TYPELIB_UUID "7FB31A56-DB6A-43EF-8C50-64269A786DA0"
 #define TYPELIB_NAME "MGA Interpreter TypeLibrary (DQML)"
-#define COCLASS_UUID "34DA5EC5-548D-4614-B338-A2E96CF6B1E0"
-#define COCLASS_NAME "MGA Interpreter CoClass (DQML)"
-#define COCLASS_PROGID "MGA.Interpreter.DQML"
+//#define COCLASS_UUID "34DA5EC5-548D-4614-B338-A2E96CF6B1E0"
+#define COCLASS_UUID "86F02B3A-37AB-4926-9EC5-3B8EDA7694BB"
+#define COCLASS_NAME "MGA Interpreter CoClass (DQML DBE)"
+#define COCLASS_PROGID "MGA.Interpreter.DQML.DBE"
 
 
 // This name will appear in the popup window for interpreter selection.
-#define COMPONENT_NAME "DQML"
+#define COMPONENT_NAME "DQML DBE"
 
 
 // This text will appear in the toolbar icon tooltip and in the menu.
-#define TOOLTIP_TEXT "DQML"
+#define TOOLTIP_TEXT "DQML DBE"
 
 
 // This #define determines the interpreter type:
@@ -39,14 +40,14 @@
 // not defined: #define REGISTER_SYSTEMWIDE
 
 // Just to please the whims of those Microsoft jerks:
-#define COCLASS_UUID_EXPLODED1 0x34DA5EC5
-#define COCLASS_UUID_EXPLODED2  0x548D
-#define COCLASS_UUID_EXPLODED3  0x4614
-#define COCLASS_UUID_EXPLODED4  0xB3
-#define COCLASS_UUID_EXPLODED5  0x38
-#define COCLASS_UUID_EXPLODED6  0xA2
-#define COCLASS_UUID_EXPLODED7  0xE9
-#define COCLASS_UUID_EXPLODED8  0x6C
-#define COCLASS_UUID_EXPLODED9  0xF6
-#define COCLASS_UUID_EXPLODED10  0xB1
-#define COCLASS_UUID_EXPLODED11  0xE0
+#define COCLASS_UUID_EXPLODED1 0x86F02B3A
+#define COCLASS_UUID_EXPLODED2  0x37AB
+#define COCLASS_UUID_EXPLODED3  0x4926
+#define COCLASS_UUID_EXPLODED4  0x9E
+#define COCLASS_UUID_EXPLODED5  0xC5
+#define COCLASS_UUID_EXPLODED6  0x3B
+#define COCLASS_UUID_EXPLODED7  0x8E
+#define COCLASS_UUID_EXPLODED8  0xDA
+#define COCLASS_UUID_EXPLODED9  0x76
+#define COCLASS_UUID_EXPLODED10  0x94
+#define COCLASS_UUID_EXPLODED11  0xBB
--- CoSMIC/PSM/DQML/interpreters/XML/ComponentConfig.h
+++ CoSMIC/PSM/DQML/interpreters/XML/ComponentConfig.h
@@ -8,17 +8,18 @@
 
 #define TYPELIB_UUID "7FB31A56-DB6A-43EF-8C50-64269A786DA0"
 #define TYPELIB_NAME "MGA Interpreter TypeLibrary (DQML)"
-#define COCLASS_UUID "34DA5EC5-548D-4614-B338-A2E96CF6B1E0"
-#define COCLASS_NAME "MGA Interpreter CoClass (DQML)"
-#define COCLASS_PROGID "MGA.Interpreter.DQML"
+//#define COCLASS_UUID "34DA5EC5-548D-4614-B338-A2E96CF6B1E0"
+#define COCLASS_UUID "1823CCC1-1661-43dc-AAB1-903E31759198"
+#define COCLASS_NAME "MGA Interpreter CoClass (DQML XML)"
+#define COCLASS_PROGID "MGA.Interpreter.DQML.XML"
 
 
 // This name will appear in the popup window for interpreter selection.
-#define COMPONENT_NAME "DQML"
+#define COMPONENT_NAME "DQML XML"
 
 
 // This text will appear in the toolbar icon tooltip and in the menu.
-#define TOOLTIP_TEXT "DQML"
+#define TOOLTIP_TEXT "DQML XML"
 
 
 // This #define determines the interpreter type:
@@ -39,14 +40,14 @@
 // not defined: #define REGISTER_SYSTEMWIDE
 
 // Just to please the whims of those Microsoft jerks:
-#define COCLASS_UUID_EXPLODED1 0x34DA5EC5
-#define COCLASS_UUID_EXPLODED2  0x548D
-#define COCLASS_UUID_EXPLODED3  0x4614
-#define COCLASS_UUID_EXPLODED4  0xB3
-#define COCLASS_UUID_EXPLODED5  0x38
-#define COCLASS_UUID_EXPLODED6  0xA2
-#define COCLASS_UUID_EXPLODED7  0xE9
-#define COCLASS_UUID_EXPLODED8  0x6C
-#define COCLASS_UUID_EXPLODED9  0xF6
-#define COCLASS_UUID_EXPLODED10  0xB1
-#define COCLASS_UUID_EXPLODED11  0xE0
+#define COCLASS_UUID_EXPLODED1 0x1823CCC1
+#define COCLASS_UUID_EXPLODED2  0x1661
+#define COCLASS_UUID_EXPLODED3  0x43dc
+#define COCLASS_UUID_EXPLODED4  0xAA
+#define COCLASS_UUID_EXPLODED5  0xB1
+#define COCLASS_UUID_EXPLODED6  0x90
+#define COCLASS_UUID_EXPLODED7  0x3E
+#define COCLASS_UUID_EXPLODED8  0x31
+#define COCLASS_UUID_EXPLODED9  0x75
+#define COCLASS_UUID_EXPLODED10  0x91
+#define COCLASS_UUID_EXPLODED11  0x98
