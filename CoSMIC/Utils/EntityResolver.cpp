10596016f72955045e53d25490201ec0f6376730
Jeff Parsons <j.parsons@vanderbilt.edu>
2009-09-18 18:35:03 +0000

    ChangeLogTag: Fri Sep 18 18:32:49 UTC 2009  Jeff Parsons <j.parsons@vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@1975 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/Utils/EntityResolver.cpp
+++ CoSMIC/Utils/EntityResolver.cpp
@@ -25,26 +25,25 @@
 
 namespace Utils
 {
+  EntityResolver::EntityResolver (const char *path)
+  : base_path_ (path)
+  {
 
-EntityResolver::EntityResolver (const char *path)
-: base_path_ (path)
-{
+  }
 
-}
+  /// This function is called by the Xerces infrastructure to
+  /// actually resolve the location of a schema.
+  xercesc::InputSource *
+  EntityResolver::resolveEntity (const XMLCh *const /* publicId */,
+                                 const XMLCh *const systemId)
+  {
+    ACE_CString path_str (this->base_path_);
+    const char *tmp = xercesc::XMLString::transcode (systemId);
+    path_str += tmp;
+    
+    Utils::XStr path (path_str.c_str ());
 
-/// This function is called by the Xerces infrastructure to
-/// actually resolve the location of a schema.
-xercesc::InputSource *
-EntityResolver::resolveEntity (const XMLCh *const publicId,
-                               const XMLCh *const systemId)
-{
-  ACE_UNUSED_ARG (publicId);
-
-  Utils::XStr path (this->base_path_.c_str ());
-  path.append (systemId);
-
-  // Ownership of these objects is taken care of.
-  return new xercesc::LocalFileInputSource (path);
-}
-
+    // Ownership of these objects is taken care of.
+    return new xercesc::LocalFileInputSource (path);
+  }
 }
