be620140fb41f5ec7d00c52ba312999792e7ad77
Sumant Tambe <sutambe@dre.vanderbilt.edu>
2009-11-01 23:58:27 +0000

    Sun Nov  1 23:51:55 UTC 2009  Sumant Tambe  <sutambe@nospam>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2041 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/tests/MyQuoter/run_test.bat
+++ CoSMIC/tests/MyQuoter/run_test.bat
@@ -9,6 +9,18 @@ if not exist %COSMIC_ROOT%\bin\idl_to_picml.exe goto no_exe
 if not exist %COSMIC_ROOT%\bin\sed.exe          goto no_sed
 
 cd %COSMIC_ROOT%\tests\MyQuoter
+
+if exist Broker.idl                 del Broker.idl
+if exist Distributor.idl            del Distributor.idl
+if exist Stock_Base.idl             del Stock_Base.idl 
+if exist PICML_default_xme_file.xme del PICML_default_xme_file.xme
+if exist MyQuoter.xme               del MyQuoter.xme
+
+@echo Running Test #1: idl_to_picml -r .
+%COSMIC_ROOT%\bin\idl_to_picml.exe -r .
+@echo ====================================================================
+
+@echo Running Test #2: idl_to_picml -x MyQuoter Stock_Base.idl Broker.idl Distributor.idl
 copy Stock_Base\Stock_Base.idl .   > nul
 
 REM Comment out Components.idl include file using sed's search and replace
@@ -20,6 +32,7 @@ REM Remove unwanted directory paths using sed's search and replace
 
 REM Run idl_to_picml.exe on the preprocessed idl files.
 %COSMIC_ROOT%\bin\idl_to_picml.exe -x MyQuoter Stock_Base.idl Broker.idl Distributor.idl
+
 goto exit
 
 :no_sed
