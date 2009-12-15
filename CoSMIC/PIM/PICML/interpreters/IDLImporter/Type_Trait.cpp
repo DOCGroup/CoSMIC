5c1491a9f9e09ecad646483f403efa24faf3c347
James Hill <hillj@cs.iupui.edu>
2009-12-15 20:26:26 +0000

    Tue Dec 15 20:25:55 UTC 2009  James H. Hill  <hillj@dre.vanderbilt.edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2099 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/PICML/interpreters/IDLImporter/Type_Trait.cpp
@@ -0,0 +1,30 @@
+// $Id$
+
+#include "Type_Trait.h"
+
+namespace IDML
+{
+const char * Type_Trait <IDML::TypeKind>::metaname = "TypeKind";
+
+const char * Type_Trait <IDML::TypeEncoding>::metaname = "TypeEncoding";
+
+const char * Type_Trait <IDML::String>::metaname = "String";
+
+const char * Type_Trait <Boolean>::metaname = "Boolean";
+
+const char * Type_Trait <ShortInteger>::metaname = "ShortInteger";
+
+const char * Type_Trait <FloatNumber>::metaname = "FloatNumber";
+
+const char * Type_Trait <DoubleNumber>::metaname = "DoubleNumber";
+
+const char * Type_Trait <LongInteger>::metaname = "LongInteger";
+
+const char * Type_Trait <GenericValueObject>::metaname = "GenericValueObject";
+
+const char * Type_Trait <GenericValue>::metaname = "GenericValue";
+
+const char * Type_Trait <GenericObject>::metaname = "GenericObject";
+
+const char * Type_Trait <Byte>::metaname = "Byte";
+}
