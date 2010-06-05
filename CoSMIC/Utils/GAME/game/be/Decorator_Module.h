fefc5b2a468cc13d1bb1a79525d8eb09eec5c1ac
James Hill <hillj@cs.iupui.edu>
2010-06-05 17:19:14 +0000

    Sat Jun  5 17:18:02 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2212 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/Utils/GAME/game/be/Decorator_Module.h
+++ CoSMIC/Utils/GAME/game/be/Decorator_Module.h
@@ -11,10 +11,12 @@ Decorator_Module _AtlModule; \
 class Decorator_App : public CWinApp { \
 public: \
   virtual BOOL InitInstance (void) { \
+    AFX_MANAGE_STATE (AfxGetStaticModuleState ()) \
     _AtlModule.InitLibId (); \
     return CWinApp::InitInstance(); \
   } \
   virtual int ExitInstance (void) { \
+    AFX_MANAGE_STATE (AfxGetStaticModuleState ())\
     _AtlModule.Term (); \
     return CWinApp::ExitInstance(); \
   } \
@@ -23,8 +25,8 @@ public: \
 BEGIN_MESSAGE_MAP (Decorator_App, CWinApp) \
 END_MESSAGE_MAP() \
 Decorator_App theApp; \
-} \
-STDAPI DllCanUnloadNow (void) { return (::AfxDllCanUnloadNow () == S_OK && GAME::_AtlModule.GetLockCount () == 0) ? S_OK : S_FALSE; } \
-STDAPI DllGetClassObject (REFCLSID rclsid, REFIID riid, LPVOID* ppv) { return GAME::_AtlModule.GetClassObject (rclsid, riid, ppv); } \
+}  \
+STDAPI DllCanUnloadNow (void) { return (::AfxDllCanUnloadNow () == S_OK && ::GAME::_AtlModule.GetLockCount () == 0) ? S_OK : S_FALSE; } \
+STDAPI DllGetClassObject (REFCLSID rclsid, REFIID riid, LPVOID* ppv) { return ::GAME::_AtlModule.GetClassObject (rclsid, riid, ppv); } \
 STDAPI DllRegisterServer (void) { return GAME::_AtlModule.DllRegisterServer (); } \
 STDAPI DllUnregisterServer (void) { return GAME::_AtlModule.DllUnregisterServer (); }
