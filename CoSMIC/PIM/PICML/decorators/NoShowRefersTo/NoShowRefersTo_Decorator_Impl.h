0bd55ddc4375932c7062c41b2b35f1c7008704df
James Hill <hillj@cs.iupui.edu>
2010-06-12 06:08:23 +0000

    Sat Jun 12 06:06:46 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2234 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/PIM/PICML/decorators/NoShowRefersTo/NoShowRefersTo_Decorator_Impl.h
@@ -0,0 +1,65 @@
+//  -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file       Component_Decorator_Impl.h
+ *
+ *  $Id$
+ *
+ *  @author     James H. Hill
+ */
+//=============================================================================
+
+#ifndef _COMPONENT_DECORATOR_IMPL_H_
+#define _COMPONENT_DECORATOR_IMPL_H_
+
+#include "game/be/Decorator_T.h"
+#include "game/be/Decorator_Impl.h"
+
+#include "game/MetaAspect.h"
+#include "game/Part.h"
+#include "game/graphics/Image_Manager_T.h"
+#include "game/graphics/Image_Resolver.h"
+
+#include "NoShowRefersTo_Decorator_export.h"
+
+namespace Gdiplus
+{
+class Bitmap;
+}
+
+/**
+ * @class NoShowRefersTo_Decorator_Impl
+ */
+class NoShowRefersTo_Decorator_Impl :
+  public GAME::Decorator_Impl
+{
+public:
+  /// Default constructor
+  NoShowRefersTo_Decorator_Impl (void);
+
+  /// Destructor.
+  ~NoShowRefersTo_Decorator_Impl (void);
+
+  int initialize_ex (const GAME::Project & proj, 
+                     const GAME::Meta::Part & part, 
+                     const GAME::FCO & fco,
+                     IMgaCommonDecoratorEvents * eventSink, 
+                     ULONGLONG parentWnd);
+
+  /// Destory the decorator.
+  void destroy (void);
+
+  int get_preferred_size (long & sx, long & sy);
+
+  /// Draw the component. This will draw the component's ports
+  /// and the components label.
+  int draw (Gdiplus::Graphics & g);
+
+private:
+  std::auto_ptr <Gdiplus::Bitmap> bitmap_;
+
+  std::string label_;
+};
+
+#endif  // !defined _PORT_LAYOUT_DECORATOR_IMPL_H_
---
+++ CoSMIC/PIM/PICML/decorators/NoShowRefersTo/NoShowRefersTo_Decorator_Module.cpp
@@ -0,0 +1,7 @@
+// $Id$
+
+#include "StdAfx.h"
+#include "game/be/Decorator_Module.h"
+#include "NoShowRefersTo_Decorator_i.c"
+
+DECLARE_DECORATOR_MODULE (LIBID_NoShowRefersTo_DecoratorLib, 101, "{07E48FA4-C7FC-4270-8583-5A58511E7A55}");
