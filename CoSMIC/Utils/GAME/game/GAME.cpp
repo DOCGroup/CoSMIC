7fb4c9d91d6c97d57a18485d367bb90efe79011a
James Hill <hillj@cs.iupui.edu>
2010-06-13 19:20:22 +0000

    Sun Jun 13 19:19:45 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2241 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/Utils/GAME/game/GAME.cpp
@@ -0,0 +1,39 @@
+// $Id$
+
+#include "stdafx.h"
+#include "GAME.h"
+#include "Exception.h"
+
+namespace GAME
+{
+  class GME_Initializer
+  {
+  public:
+    GME_Initializer (void)
+    {
+      if (!GME_Initializer::is_init_)
+      {
+        VERIFY_HRESULT (::CoInitializeEx (0, COINIT_APARTMENTTHREADED));
+        GME_Initializer::is_init_ = true;
+      }
+    }
+
+    /// Default constructor
+    ~GME_Initializer (void)
+    {
+      if (GME_Initializer::is_init_)
+      {
+        ::CoUninitialize ();
+        GME_Initializer::is_init_ = false;
+      }
+    }
+
+  private:
+    /// Flag that determine is GME is initialized.
+    static int is_init_;
+  };
+
+  int GME_Initializer::is_init_ = false;
+}
+
+static GAME::GME_Initializer init;
