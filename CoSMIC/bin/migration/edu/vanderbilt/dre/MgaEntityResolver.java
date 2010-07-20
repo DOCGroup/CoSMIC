64a8ead0d3aace927554992864f7567e2f046630
James Hill <hillj@cs.iupui.edu>
2010-07-20 20:31:29 +0000

    Tue Jul 20 20:21:39 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2380 93964bc7-a711-0410-b8d6-de67a5477bd1

---
+++ CoSMIC/bin/migration/edu/vanderbilt/dre/MgaEntityResolver.java
@@ -0,0 +1,56 @@
+// -*- Java -*-
+
+//=============================================================================
+/**
+ * @file        MgaEntityResolver.java
+ * 
+ * $Id$
+ * 
+ * @author      James H. Hill
+ */
+//=============================================================================
+
+package edu.vanderbilt.dre;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import java.io.File;
+
+/**
+ * @class GmeEntityResolver
+ * 
+ * Implementation of an EntityResolver for migrating GME models. The
+ * resolver is hardcoded to locate mga.dtd in the following location:
+ * 
+ *    $COSMIC_ROOT/includes/GME/mga.dtd
+ */
+public class MgaEntityResolver implements EntityResolver
+{
+  /**
+   * Callback method for resolving the entity
+   * 
+   * @param[in]       publicId        not used
+   * @param[in]       systemId        System id to resolve
+   * @return          The entity resolver as an input source.
+   */
+  public InputSource resolveEntity (String publicId, String systemId)
+  {
+    try
+    {
+      // We only care about the mga.dtd systemId.
+      if (!systemId.endsWith ("mga.dtd"))
+        return null;
+  
+      // Get the COSMIC_ROOT environment variable. Use it to 
+      // construct the location of mga.dtd.
+      final String COSMIC_ROOT = System.getenv ("COSMIC_ROOT");
+      final String mgaPath = "file:///" + COSMIC_ROOT + "/includes/GME/mga.dtd";
+      
+      return new InputSource (mgaPath);
+    }
+    catch (Exception ex)
+    {
+      ex.printStackTrace ();
+      return null;
+    }
+  }
+}
