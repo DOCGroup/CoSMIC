72cab1a5f0151d349f4214e9ae014841766b5acf
alhad <alhad>
2010-08-10 14:32:02 +0000

    Tue Aug 10 14:29:05 UTC 2010  Alhad Mokashi  <amokashi at iupui dot edu>
    git-svn-id: file:///project/Git-Conversion/CoSMIC/trunk@2442 93964bc7-a711-0410-b8d6-de67a5477bd1

--- CoSMIC/PIM/PICML/tests/IDL_to_PICML/Enum.cpp
+++ CoSMIC/PIM/PICML/tests/IDL_to_PICML/Enum.cpp
@@ -17,15 +17,15 @@ int ACE_TMAIN (int argc, ACE_TCHAR * argv [])
   ACE_DEBUG ((LM_DEBUG, "*** begin validating enum type\n"));
 
   // verify enum value
-  if (ec1 == 0)
+  if (ec1 != 0)
     ACE_ERROR ((LM_ERROR, "Value of Enum set incorrectly\n"));
 
   // verify enum value
-  if (ec2 == 1)
+  if (ec2 != 1)
     ACE_ERROR ((LM_ERROR, "Value of Enum set incorrectly\n"));
 
   // verify enum value
-  if (ec3 == 2)
+  if (ec3 != 2)
     ACE_ERROR ((LM_ERROR, "Value of Enum set incorrectly\n"));
 
   ACE_DEBUG ((LM_DEBUG, "*** end validating eum type\n"));
