22522cc09637b5d17df7080eba69db88e2420385
James H. Hill <hillj@cs.iupui.edu>
2014-07-29 21:08:02 -0400

    Fixed compilation errors

--- Utils/UDM/visit.cpp
+++ Utils/UDM/visit.cpp
@@ -2,7 +2,6 @@
 
 #include "UdmBase.h"
 #include <algorithm>
-#include "boost/bind.hpp"
 
 namespace CoSMIC
 {
@@ -11,16 +10,17 @@ namespace CoSMIC
 		template <typename T, typename V>
 		void visit_all (T coll, V & v) 
 		{
-			std::for_each (coll.begin (), 
-				coll.end (),
-				boost::bind (&T::value_type::Accept, _1, boost::ref (v)));
+      std::for_each (coll.begin (), coll.end (), [&] (T::value_type item) {
+        item.Accept (v);
+      });
 		}
 
 		template <typename T, typename P, typename V>
 		void visit_all (const P & parent, V & v) 
 		{
-			typedef vector <T> vector_t;
-			vector_t children = UDM_NAMESPACE::ChildrenAttr <T> (parent.__impl (), UDM_NAMESPACE::NULLCHILDROLE);
+			std::vector <T> children =
+        ::UDM_NAMESPACE::ChildrenAttr <T> (parent.__impl (), 
+                                           ::UDM_NAMESPACE::NULLCHILDROLE);
 
 			visit_all (children, v);
 		}
