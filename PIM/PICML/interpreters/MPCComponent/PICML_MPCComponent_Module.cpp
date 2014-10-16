94852dafb93af8e9a45d05052420708a3de60a5d
Dennis Feiock <dfeiock@iupui.edu>
2014-10-16 10:53:52 -0400

    Removed UDM implemetation and replaced with GAME implementation.  Solution currently compiles.

---
+++ PIM/PICML/interpreters/MPCComponent/PICML_MPCComponent_Module.cpp
@@ -0,0 +1,10 @@
+// $Id$
+
+#include "StdAfx.h"
+#include "game/mga/component/Component_Module.h"
+#include "PICML_MPCComponent_i.c"
+
+DECLARE_COMPONENT_MODULE ("PICML_MPC",
+                          LIBID_PICML_MPCComponent,
+                          1000,
+                          "{74255f9a-634e-4fb7-a983-d977cd6e6dc0}");
